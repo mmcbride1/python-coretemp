8d3a220a1256208c6951aadd67cf2c8659e9d8c4
mmcbride1 <mrmcbride@smcm.edu>
2016-11-23 14:01:54 -0500

    added functions to sensor_reading  .

--- sensor_reading.py
+++ sensor_reading.py
@@ -27,6 +27,44 @@ class SensorReading:
    ERRO = log.ExceptionLog()
 
    def __init__(self):
+      """ """
       try:
       except Exception as ex:
          ERRO.update_errlog(ex)
+
+   def get_reading(self):
+      """ """
+      return self.read
+
+   def get_message(self):
+      """ """
+      return self.MSG    
+
+   def get_failed(self):
+      """ """
+      return re.sub(".*NORMAL.*\n?","",self.MSG)
+
+   def __collect_default(self, sub):
+      """ """
+      self.sub = sub
+
+      num = sub.get_value()   
+      if sub.type == SUB_MAX_TYPE:
+         self.high.append(num)
+      if sub.type == SUB_CRT_TYPE:
+         self.crit.append(num)
+
+   def __avg(self, arr):
+      """ """
+      self.arr = arr
+      
+      try:
+         avg = sum(arr)/float(len(arr)) 
+         return round(avg, 2)
+      except ZeroDivisionError as z:
+         ERRO.update_errlog(z)
+         return 0
+
+   def get_avg_read(self):
+      """ """
+      return self.avg(self.read.values())

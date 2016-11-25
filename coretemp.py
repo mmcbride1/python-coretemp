221a6eeef8ebaec866c9520cd004a724abe4f950
mmcbride1 <mrmcbride@smcm.edu>
2016-11-24 23:25:20 -0500

    coretemp script

---
+++ coretemp.py
@@ -0,0 +1,43 @@
+import sys
+import coretemp_log as log
+import sensor_reading as r
+import coretemp_alert as alert 
+
+''' '''
+read = r.SensorReading()
+
+''' '''
+msg = read.get_message()
+
+''' '''
+ctlog = log.CoretempLog()
+exlog = log.ExceptionLog()
+
+def write_avg():
+   """ """
+   core_avg = read.get_avg_read()
+   return "\nCore Mean: %s\n" % core_avg
+
+def send(_msg):
+   """ """
+   ar = alert.Alert()
+   ar.send_message(_msg)
+
+def monitor(_msg):
+   """ """
+   if _msg:
+      x = read.get_failed()
+      _msg += write_avg()
+      ctlog.update_templog(_msg)
+      if x:
+         send(x)
+
+def main(_msg):
+   """ """
+   try:
+      monitor(_msg)
+   except Exception as ex:
+      exlog.update_errlog(ex)
+
+if __name__ == "__main__":
+   main(msg)

ee1c047575722886658c1a765eb115905d69d103
mmcbride1 <mrmcbride@smcm.edu>
2016-11-25 22:23:34 -0500

    updated objects in main script

--- coretemp.py
+++ coretemp.py
@@ -4,43 +4,56 @@ import coretemp_log as log
 import sensor_reading as r
 import coretemp_alert as alert 
 
-''' '''
-read = r.SensorReading()
-
-''' '''
-msg = read.get_message()
-
-''' '''
+''' Main logs '''
 ctlog = log.CoretempLog()
 exlog = log.ExceptionLog()
 
-def write_avg():
-   """ """
+def write_avg(read):
+   """ 
+   Helper function for
+   getting mean value from sensor
+   read and writing the output str
+   :param SensorReading read: sensor read
+   :return: core mean message to append
+   """
    core_avg = read.get_avg_read()
    return "\nCore Mean: %s\n" % core_avg
 
-def send(_msg):
-   """ """
+def send(msg_):
+   """ 
+   Send the alert message where
+   applicable
+   :param str msg_: the message to send
+   """
    ar = alert.Alert()
-   ar.send_message(_msg)
+   ar.send_message(msg_)
 
-def monitor(_msg):
-   """ """
-   if _msg:
+def monitor():
+   """ 
+   Post to the log file and
+   send the alert message where
+   applicable 
+   """
+   read = r.SensorReading()
+   msg_ = read.get_message()
+ 
+   if msg_:
       x = read.get_failed()
-      _msg += write_avg()
-      ctlog.update_templog(_msg)
+      msg_ += write_avg()
+      ctlog.update_templog(msg_)
       if x:
          send(x)
 
-def main(_msg):
-   """ """
+def main():
+   """ 
+   Main
+   """
    try:
-      monitor(_msg)
+      monitor()
    except Exception as ex:
       exlog.update_errlog(ex)
 
 if __name__ == "__main__":
    while True:
-      main(msg)
+      main()
       time.sleep(120)

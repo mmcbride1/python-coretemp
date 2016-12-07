2360ed16718ae75db8c26a7f7da07933c178e08f
mmcbride1 <mrmcbride@smcm.edu>
2016-12-07 13:32:09 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,34 +1,50 @@
 import os
 import shutil
-import subprocess 
-from setuptools import setup    
+import subprocess
+from setuptools import setup
+from pkg_resources import resource_string
 from setuptools.command.install import install
 
+''' coretemp - setup.py '''
+
 class PostInstallCommand(install):
-   
-   def mv_properties():
-      b = 'coretemp/'
-      f = 'coretemp.properties'
-      shutil.copy2(b + f, '/etc/')
+   """ 
+   Post installation command to
+   copy the daemon properties file.    
+   """
+
+   def mv_properties(self):
+      """ 
+      Copy the properties file 
+      to the conf directory.
+      """
+      f = '/coretemp/coretemp.properties'
+      b = os.getcwd()
+      shutil.copy2(b+f, '/etc/')
 
    def run(self):
+      """ 
+      Run (override).
+      """
+      self.mv_properties()
       install.run(self)
-   
+
+# setup # 
 setup(
    name = 'coretemp',
    packages = ['coretemp'],
-   version = '0.1.3',
+   version = '0.1',
    install_requires = [
     'pysensors >= 0.0.3',
     'termcolor >= 0.0.1',
    ],
-   description = 'A simple service to monitor CPU temp',
+   package_data = {'coretemp': ['*.properties']},
+   description = 'lightweight service to monitor CPU temp',
    author = 'Matthew R McBride',
    author_email = 'mrmcbride@smcm.edu',
    url = 'https://github.com/mmcbride1/python-coretemp',
    classifiers = [],
-#   cmdclass = {'install': PostInstallCommand},
-   data_files = [('/etc', ['coretemp.properties'])],
+   cmdclass = {'install': PostInstallCommand},
    entry_points = {
    'console_scripts': [
    'coretemp-monitor=monitor:cli',
